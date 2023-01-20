d4b81eb440c6bb8da61a92fe9c1af30ea157368d
Jessthesaver <70416069+Jessthesaver@users.noreply.github.com>
2023-01-20 16:00:54 -0600

    Add files via upload

--- Problem3.js
+++ Problem3.js
@@ -1,10 +1,15 @@
 function lettershift(str) {
-  let regex = /([a-zA-Z0-9])/gm;
-  let match=str.match(regex);
-  let switched=[];
-  match.forEach(element => {
-    switched.push(String.fromCharCode(element.charCodeAt()+1))
+  const switched=str.replace(/([a-zA-Z0-9])/gm , element => {
+    if(element.charCodeAt()==57){//problem characters code
+      return '0'
+    }else if(element.charCodeAt()==122){
+      return 'a'
+    }else if(element.charCodeAt()==90){
+      return 'A'
+    }else{
+      return String.fromCharCode(element.charCodeAt()+1)
+    }   
   });   
-  return switched.join('');
+  return switched;
 }
-console.log(lettershift("es2f3"));
+console.log(lettershift("es2f3z9Z0"));
